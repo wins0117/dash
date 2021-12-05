eee0300ffd85c8c793c1f27ef0266ee4ff185589
wins0117 <32445436+wins0117@users.noreply.github.com>
2021-12-05 22:33:56 +0800

    Update static-image-export.py

--- static-image-export.py
+++ static-image-export.py
@@ -43,4 +43,4 @@ def display_graph(render_option):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run_server(host = "0,0,0,0", port = port)
+    app.run_server(host = "0.0.0.0", port = port)
